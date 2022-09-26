1bca4d12547f069e4f1b54f108e1b86cef170acd
artemiygusev04 <PMPO1002@yandex.ru>
2022-09-26 21:30:06 +0300

    s

--- tests/android/patched_style/test_wikipedia.py
+++ tests/android/patched_style/test_wikipedia.py
@@ -8,23 +8,17 @@ from mobile_tests_lesson_13.model import app
 def test_search():
     app.given_opened()
 
-    with title('Search for content'):
-        browser.element('Search Wikipedia').tap()
-        browser.element('#search_src_text').type('BrowserStack')
+    with title('Check tab explore'):
+        browser.element('Search Wikipedia').should(be.visible)
 
-    with title('Content should be found'):
-        browser.all('#page_list_item_title').should(have.size_greater_than(0))
-        browser.element('«Software company based in India»').should(be.visible)
+    with title('Check tab saved'):
+        browser.element('Saved').tap()
+        browser.element('#main_toolbar').element('«Saved»').should(be.visible)
 
+    with title('Check tab search'):
+        browser.element('Search').tap()
+        browser.element('#history_title').should(have.text('History'))
 
-def test_search_is_efficient_enough_to_find_selene_as_python_package():
-    app.given_opened()
-
-    with title('Search for content'):
-        browser.element('Search Wikipedia').tap()
-        browser.element('#search_src_text').type('Selene')
-
-    with title('Content should be found'):
-        browser.all('#page_list_item_title').first.should(
-            have.text('User-oriented Web UI browser tests in Python')
-        )
+    with title('Check tab edits'):
+        browser.element('Edits').tap()
+        browser.element('#main_toolbar').element('«Edits»').should(be.visible)
